d9d4d2f76bfdb24d48a28b160a6db1fb06db0cd8
jetLee92 <940077208@qq.com>
2018-08-03 18:35:34 +0800

    添加左边图标attrs

--- app/src/main/java/com/jetlee/simpleedittext/SampleEditText.java
+++ app/src/main/java/com/jetlee/simpleedittext/SampleEditText.java
@@ -1,27 +1,35 @@
 package com.jetlee.simpleedittext;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
 
 /**
- * Created by Administrator on 2018/8/3.
+ * @author：Jet啟思
+ * @date:2018/8/3 10:02
  */
-
 public class SampleEditText extends AppCompatEditText {
 
     private Paint textPaint;
+    private Paint linePaint;
     // hint字体大小
     private static float hintSize = Utils.dpToPx(14);
     // label与输入框的距离
-    private static float OFFSET_LABEL = Utils.dpToPx(10);
+    private static float OFFSET_LABEL = Utils.dpToPx(8);
+    // 左边距
+    private static float OFFSET_LABEL_LEFT = Utils.dpToPx(4);
     // label底部距离最顶部的距离
-    private static float OFFSET_LABEL_TOP = Utils.dpToPx(12);
+    private static float OFFSET_LABEL_TOP = Utils.dpToPx(0);
     // label是否显示，根据字数判断
     private boolean isLabelShow;
     // 动画系数
@@ -30,38 +38,71 @@ public class SampleEditText extends AppCompatEditText {
     // hint动画的距离
     private float animatorDistance = OFFSET_LABEL + hintSize;
 
+    // 左边图标ID
+    private int leftIconId;
+    private Bitmap leftIconBitmap;
+    private int leftIconSize = (int) Utils.dpToPx(24);
+
     public SampleEditText(Context context) {
         super(context);
     }
 
     public SampleEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initAttr(attrs);
         init();
     }
 
     public SampleEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initAttr(attrs);
         init();
     }
 
+    private void initAttr(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.SampleEditText);
+        leftIconId = typedArray.getResourceId(R.styleable.SampleEditText_leftIcon, -1);
+        leftIconBitmap = Utils.getBitmap(getResources(), leftIconSize, leftIconId);
+        typedArray.recycle();
+    }
+
+    @SuppressLint("NewApi")
     private void init() {
-        setPadding(getPaddingLeft(), (int) (getPaddingTop() + OFFSET_LABEL + hintSize + OFFSET_LABEL_TOP), getPaddingRight(), getPaddingBottom());
+
+        setBackground(null);
+        float leftPadding = 0;
+        if (leftIconId != -1 && leftIconBitmap != null) {
+            leftPadding = leftIconSize + Utils.dpToPx(6);
+        }
+        setPadding((int) (OFFSET_LABEL_LEFT + leftPadding), (int) (getPaddingTop() + OFFSET_LABEL + hintSize + OFFSET_LABEL_TOP), getPaddingRight(), getPaddingBottom());
         animatorDistance = OFFSET_LABEL + getTextSize();
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setTextSize(hintSize);
+
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(getResources().getColor(R.color.colorAccent));
+        linePaint.setStrokeWidth(Utils.dpToPx(1));
         setTextChangedListener();
+        setFocusChangeListener();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        float leftPadding = 0;
+        // 画左边icon
+        if (leftIconId != -1 && leftIconBitmap != null) {
+            canvas.drawBitmap(leftIconBitmap, OFFSET_LABEL_LEFT, getPaddingTop(), linePaint);
+            leftPadding = leftIconSize + Utils.dpToPx(6);
+        }
+        // 画hint
         CharSequence hint = getHint();
         textPaint.setAlpha((int) (labelFraction * 0xff));
-//        textPaint.setTextSize();
-        canvas.drawText(hint, 0, hint.length(), getPaddingLeft(),
-                hintSize + OFFSET_LABEL_TOP + (1 - labelFraction) * animatorDistance, textPaint);
-
+        canvas.drawText(hint, 0, hint.length(), OFFSET_LABEL_LEFT + leftPadding,
+                getPaddingTop() - OFFSET_LABEL + (1 - labelFraction) * animatorDistance, textPaint);
+        // 画下划线
+        canvas.drawLine(OFFSET_LABEL_LEFT + leftPadding, getBottom() - Utils.dpToPx(8),
+                getWidth() - getPaddingRight(), getBottom() - Utils.dpToPx(8), linePaint);
     }
 
     private void setTextChangedListener() {
@@ -84,6 +125,21 @@ public class SampleEditText extends AppCompatEditText {
                 } else if (s.length() == 0) {
                     isLabelShow = false;
                     getAnimator().reverse();
+                }
+            }
+        });
+    }
+
+    private void setFocusChangeListener() {
+        setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    linePaint.setColor(getResources().getColor(R.color.colorAccent));
+                    linePaint.setStrokeWidth(Utils.dpToPx(2));
+                } else {
+                    linePaint.setColor(Color.parseColor("#666666"));
+                    linePaint.setStrokeWidth(Utils.dpToPx(1));
                 }
             }
         });
--- app/src/main/java/com/jetlee/simpleedittext/Utils.java
+++ app/src/main/java/com/jetlee/simpleedittext/Utils.java
@@ -1,6 +1,8 @@
 package com.jetlee.simpleedittext;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.TypedValue;
 
 /**
@@ -11,6 +13,16 @@ public class Utils {
 
     public static float dpToPx(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
+    }
+
+    public static Bitmap getBitmap(Resources res, int size, int resId) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        options.inJustDecodeBounds = false;
+        options.inDensity = Math.min(options.outWidth, options.outHeight);
+        options.inTargetDensity = size;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 
 }
